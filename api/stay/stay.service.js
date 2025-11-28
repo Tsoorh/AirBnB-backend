e826acc3004c0a85b417b480da6bec6a6c37674d
Tsoorh <Tsoor.hartov@gmail.com>
2025-11-28 08:35:16 +0200

    stay service CRUDL done

--- api/stay/stay.service.js
+++ api/stay/stay.service.js
@@ -2,22 +2,23 @@ import { ObjectId } from "mongodb";
 import { dbService } from "../../services/db.service.js";
 import { loggerService } from "../../services/logger.service.js";
 
-const COLLECTION ='stay';
+const COLLECTION = 'stay';
 
 
 export const stayService = {
     query,
     getById,
     add,
-    update
+    update,
+    remove
 }
 
 async function query(filterBy) {
     try {
-        const collection=await dbService.getCollection(COLLECTION);
+        const collection = await dbService.getCollection(COLLECTION);
         const criteria = _getCriteria(filterBy)
         const stayCursor = await collection.find(criteria);
-        
+
         const stays = stayCursor.toArray();
         return stays
     } catch (err) {
@@ -26,12 +27,12 @@ async function query(filterBy) {
     }
 }
 
-async function getById(stayId){
+async function getById(stayId) {
     try {
-        const collection=await dbService.getCollection(COLLECTION);
-        const criteria= {_id:ObjectId.createFromHexString(stayId)}
-        
-        const stay =await collection.findOne(criteria);
+        const collection = await dbService.getCollection(COLLECTION);
+        const criteria = { _id: ObjectId.createFromHexString(stayId) }
+
+        const stay = await collection.findOne(criteria);
         return stay
     } catch (err) {
         loggerService.error("Couldn't get stay by id")
@@ -39,64 +40,81 @@ async function getById(stayId){
     }
 }
 
-async function add(stay){
+async function add(stay) {
     try {
         const collection = await dbService.getCollection(COLLECTION)
-        const res =await collection.insertOne(stay)
-
-        if(!res.acknowledged) return {success:false,message:"Not added"}
+        const res = await collection.insertOne(stay)
+        if (!res.acknowledged) throw new Error("Couldn't add stay")
 
         stay._id = res.insertedId
-        
     } catch (err) {
         loggerService.error("Couldn't add stay")
         throw err
     }
 }
 
-async function update(stay){
+async function update(stay) {
     try {
         const collection = await dbService.getCollection(COLLECTION)
-        const criteria = {_id:ObjectId.createFromHexString(stay._id)}
-        const {_id,...nonIdStay} = stay
-        const update = {$set:nonIdStay}
-        const res = await collection.updateOne(criteria,update)
+        const { _id, ...nonIdStay } = stay;
+        const criteria = { _id: ObjectId.createFromHexString(_id) }
 
-        if(res.matchedCount===0) return {success:false, message:"Stay not found"}
-        if(res.modifiedCount===1){
-            return {success:true, message:"Updated"}
-        } else{
-            return {success:true, message:"No changes"}
-        }
+        const stayExist = await collection.findOne(criteria)
+        if (!stayExist) throw new Error("Couldn't find stay to update")
+
+        const update = { $set: nonIdStay }
+        const res = await collection.updateOne(criteria, update)
+
+        if (!res.acknowledged) throw new Error("Couldn't update stay")
+
+        return stay
     } catch (err) {
         loggerService.error("Couldn't update stay")
         throw err
     }
 }
 
+async function remove(stayId) {
+    try {
+        const collection = await dbService.getCollection(COLLECTION);
+        const criteria = { _id: new ObjectId(stayId) }
+        const stayExist = await collection.findOne(criteria)
+        if (!stayExist) throw new Error("Couldn't find stay to remove")
+
+        const res = await collection.deleteOne(criteria);
+        if (res.deletedCount === 0) throw new Error("Couldn't delete stay")
+
+        return stayId
+    } catch (err) {
+        loggerService.error("Couldn't update stay")
+        throw err
+    }
+
+}
 
 
-function _getCriteria(filterBy){
-    const criteria ={}
 
-    if(filterBy.city){
+function _getCriteria(filterBy) {
+    const criteria = {}
+
+    if (filterBy.city) {
         criteria.city = filterBy.city
     }
-    if(filterBy.who){
-        if(filterBy.who.pets){
-            criteria.houseRules="Pets allowed (with fee)"
+    if (filterBy.who) {
+        if (filterBy.who.pets) {
+            criteria.houseRules = "Pets allowed (with fee)"
         }
-        if(filterBy.who.adults || filterBy.who.children){
+        if (filterBy.who.adults || filterBy.who.children) {
             const guests = filterBy.who?.adults + filterBy.who?.children
-            criteria.capacity = {$gte:{guests}}
+            criteria.capacity = { $gte: { guests } }
         }
     }
-    if(filterBy.dates){
+    if (filterBy.dates) {
         criteria.unavailable = {
-            $not:{
-                $elemMatch:{
-                    "startDate":{$lt: filterBy.dates.checkOut},
-                    "endDate":{$gt: filterBy.dates.checkIn},
+            $not: {
+                $elemMatch: {
+                    "startDate": { $lt: filterBy.dates.checkOut },
+                    "endDate": { $gt: filterBy.dates.checkIn },
                 }
             }
         }
